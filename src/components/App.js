f8a7136057e1d0b934e89e5d097b656514d41d3a
David Lavieri <daviddlavier@gmail.com>
2017-11-06 15:03:18 -0400

    Move home to stateless component

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
+import Home from './Home'
 import Counter from './Counter'
 
 const App = () => {
-  console.log('update app')
   return (
     <Router>
       <div>
-        <Link to='/'>Home</Link>
-        &nbsp;
-        <Link to='/counter'>Counter</Link>
+        <Link to='/'>Home</Link> | <Link to='/counter'>Counter</Link>
 
-        <Route path='/' exact render={() => (
-          <div>
-            <h1>React Starter </h1>
-          </div>
-        )} />
+        <Route path='/' exact render={Home} />
         <Route path='/counter' component={Counter} />
       </div>
     </Router>
